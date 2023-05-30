fe833e1ac5060a87c1a62cf07a079f78adc7c3b3
hariomhks1997 <130026593+hariomhks1997@users.noreply.github.com>
2023-05-30 10:58:36 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (<div>
+        <header><h1>Expense Item!</h1></header>
+ <h4>Food Rs 10</h4>
+ <h4> Petrol Rs 100</h4>
+ <h4>Movies Rs 200</h4>
+ 
+ </div>
+    )
+    
+}
+export default ExpenseItem;
