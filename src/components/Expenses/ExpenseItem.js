8d7469555ee0d7492899a980cd6143fad9f42c29
hariomhks1997 <130026593+hariomhks1997@users.noreply.github.com>
2023-06-02 13:13:25 +0530

    use state to upadate amount and title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
@@ -7,23 +8,37 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem= (props) => {
+  let [title, settitle]=useState(props.title)
+  let [amount, setamount]=useState(props.amount)
   const changehandler=()=>{
-    console.log("click")
-  }
+    settitle('updated')
+    
+}
   const deletehandler=()=>{
-    console.log("delete")
+    let deletet=document.querySelector('.tiger')
+    let div=document.querySelector('.expense-item')
+
+  
+    deletet.removeChild(div)
+    
+  
+    
+  }
+  const expensehandler=()=>{
+    setamount(100)
   }
   
     
     return (
-      
+  <div  className='tiger'>  
 <Card className='expense-item'>
   <ExpenseDate
     date={props.date}
   ></ExpenseDate>
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
+        <button onClick={expensehandler}>ExpenseAmountUpadated</button>
         <ExpenseDetails
         amount={props.amount}
         title={props.title}
@@ -34,6 +49,7 @@ const ExpenseItem= (props) => {
         
       </div>
     </Card>
+    </div>  
     
     
     )
